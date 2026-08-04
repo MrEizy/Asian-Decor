4c8f33a7f714b40e7127cc58243eeff19453e6f0
MrEizy <MrEizy@hotmail.com>
2026-08-04 09:23:44 +0200

    Jei Plugin 1.21.1 -> 26.1.2

--- src/main/java/net/zic/builders_zenith/compat/jei/JEIPlugin.java
+++ src/main/java/net/zic/builders_zenith/compat/jei/JEIPlugin.java
@@ -4,17 +4,23 @@ package net.zic.builders_zenith.compat.jei;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.helpers.IGuiHelper;
-import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.types.IRecipeType;
 import mezz.jei.api.registration.IGuiHandlerRegistration;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
-import net.minecraft.client.Minecraft;
 import net.minecraft.resources.Identifier;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeHolder;
-import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraft.world.item.crafting.RecipeInput;
+import net.minecraft.world.item.crafting.RecipeMap;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RecipesReceivedEvent;
+import net.neoforged.neoforge.event.OnDatapackSyncEvent;
 import net.zic.builders_zenith.BuildersZenith;
 import net.zic.builders_zenith.blocks.ModBlocks;
 import net.zic.builders_zenith.recipe.CarpenterRecipes;
@@ -28,11 +34,16 @@ import java.util.List;
 @JeiPlugin
 public class JEIPlugin implements IModPlugin {
 
-    public static final RecipeType<CarpenterRecipes> CARPENTER_RECIPE_TYPE =
-            RecipeType.create(BuildersZenith.MOD_ID, "carpenter", CarpenterRecipes.class);
+    // Recipes synced from the server via OnDatapackSyncEvent/RecipesReceivedEvent below.
+    // registerRecipes() runs at JEI startup, which can happen before Minecraft.getInstance().level
+    // exists (and Level#getRecipeManager was removed anyway) - the synced RecipeMap sidesteps both.
+    private static RecipeMap syncedRecipes = RecipeMap.EMPTY;
 
-    public static final RecipeType<ColorMixerRecipe> COLOR_MIXER_RECIPE_TYPE =
-            RecipeType.create(BuildersZenith.MOD_ID, "color_mixer", ColorMixerRecipe.class);
+    public static final IRecipeType<RecipeHolder<CarpenterRecipes>> CARPENTER_RECIPE_TYPE =
+            createRecipeHolderType("carpenter");
+
+    public static final IRecipeType<RecipeHolder<ColorMixerRecipe>> COLOR_MIXER_RECIPE_TYPE =
+            createRecipeHolderType("color_mixer");
 
     @Override
     public Identifier getPluginUid() {
@@ -48,26 +59,10 @@ public class JEIPlugin implements IModPlugin {
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
-        RecipeManager recipeManager = Minecraft.getInstance().level.getRecipeManager();
-
-        // Carpenter Recipes (unchanged)
-        List<CarpenterRecipes> carpenterRecipes = recipeManager
-                .getAllRecipesFor(ModRecipes.CARPENTER_TYPE.get())
-                .stream()
-                .map(RecipeHolder::value)
-                .toList();
-        registration.addRecipes(CARPENTER_RECIPE_TYPE, carpenterRecipes);
+        registration.addRecipes(CARPENTER_RECIPE_TYPE, getRecipes(syncedRecipes, ModRecipes.CARPENTER_TYPE.get()));
 
         // Color Mixer - Only 2 recipes, but show all 256 combinations via JEI's recipe filling
-        List<ColorMixerRecipe> colorMixerRecipes = recipeManager
-                .getAllRecipesFor(ModRecipes.COLOR_MIXER_TYPE.get())
-                .stream()
-                .map(RecipeHolder::value)
-                .toList();
-
-        // Register the 2 base recipes
-        registration.addRecipes(COLOR_MIXER_RECIPE_TYPE, colorMixerRecipes);
-
+        registration.addRecipes(COLOR_MIXER_RECIPE_TYPE, getRecipes(syncedRecipes, ModRecipes.COLOR_MIXER_TYPE.get()));
         // JEI will automatically expand tag ingredients to show all combinations!
     }
 
@@ -95,5 +90,35 @@ public class JEIPlugin implements IModPlugin {
                 101, 32, 24, 17,
                 COLOR_MIXER_RECIPE_TYPE
         );
+    }
+
+    // From Occultism, under MIT License
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private <I extends RecipeInput, T extends Recipe<I>> List<RecipeHolder<T>> getRecipes(RecipeMap recipeMap, RecipeType<T> type) {
+        return (List) recipeMap.byType(type);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> IRecipeType<T> createRecipeHolderType(String path) {
+        return (IRecipeType<T>) IRecipeType.create(Identifier.fromNamespaceAndPath(BuildersZenith.MOD_ID, path), RecipeHolder.class);
+    }
+
+    @EventBusSubscriber(modid = BuildersZenith.MOD_ID)
+    public static class ServerRecipeSync {
+        @SubscribeEvent
+        public static void onDatapackSync(OnDatapackSyncEvent event) {
+            event.sendRecipes(
+                    ModRecipes.CARPENTER_TYPE.get(),
+                    ModRecipes.COLOR_MIXER_TYPE.get()
+            );
+        }
+    }
+
+    @EventBusSubscriber(modid = BuildersZenith.MOD_ID, value = Dist.CLIENT)
+    public static class ClientRecipeSync {
+        @SubscribeEvent
+        public static void onRecipeReceived(RecipesReceivedEvent event) {
+            syncedRecipes = event.getRecipeMap();
+        }
     }
 }
